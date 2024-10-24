02b2e4c7d30d9a454e4546fe9d767692d5a8fe65
23r0 <125326946+GustavoAZepeda@users.noreply.github.com>
2024-10-23 22:21:12 -0600

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,16 +1,19 @@
 <?php
-const BASE_URL = "http://localhost/tienda-youtube/";
-const HOST = "localhost";
-const USER = "root";
-const PASS = "";
-const DB = "tienda-youtube";
-const CHARSET = "charset=utf8";
-const TITLE = "TIENDA ONLINE";
-const MONEDA = "USD";
-const CLIENT_ID = "";
-
-const USER_SMTP = "Tu correo";
-const PASS_SMTP = "contraseña generada para esta aplicacion";
-const PUERTO_SMTP = 465;
-const HOST_SMTP = "smtp.gmail.com";
+  // URL del proyecto (IP pública del servidor)
+  const BASE_URL = "http://3.142.122.180/";
+  // Configuración de la base de datos en Amazon RDS
+  const HOST = "tienda-youtube.c3saw6o8oztg.us-east-2.rds.amazonaws.com"; // Endpoint de Amazon RDS
+  const USER = "admin"; // Usuario de la base de datos
+  const PASS = "System22"; // Contraseña de la base de datos
+  const DB = "tienda_youtube"; // Nombre de la base de datos
+  const CHARSET = "charset=utf8"; // Charset para la base de datos
+  // Información general de la tienda
+  const TITLE = "TIENDA ONLINE";
+  const MONEDA = "USD"; // Moneda de la tienda
+  const CLIENT_ID = ""; // Opcional, si tienes un client ID para pagos o similar
+  // Configuración de SMTP para enviar correos (usando Gmail en este caso)
+  const USER_SMTP = "gzepedah2@miumg.edu.gt"; // Coloca tu dirección de correo aquí
+  const PASS_SMTP = "fxor obzh hoxv jlkw"; // Contraseña generada para la app en Gmail
+  const PUERTO_SMTP = 465; // Puerto SMTP para Gmail
+  const HOST_SMTP = "smtp.gmail.com"; // Servidor SMTP de Gmail
 ?>
